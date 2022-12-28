f9821a1d609d54e9d263fdc6d34392c2f0f9461c
Jesse Lumarie <jesse.lumarie@gmail.com>
2022-12-28 15:22:16 -0700

    Use spotify colors

--- pages/select/index.tsx
+++ pages/select/index.tsx
@@ -178,6 +178,7 @@ function SelectScreen({
               setPlaylists,
               setTracks,
               setShowToast,
+              setShowErrorToast,
             })
           }
           count={selectedCount}
